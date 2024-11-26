f0eb725f60b082735414fa59efce70ad523bac4b
MariaFerValdivia <maria.valdivia@genstudents.org>
2024-11-26 12:21:12 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+mensaje="Mi nombre es Maru"
+print(mensaje)
+
+nombre="Maru"
+print(f"Hola,{nombre}. Bienvenido/a a tamaworld.")
+
+nombre=input("Introduce tu nombre")
+print(f"hola {nombre}. Bienvendio a tamaworld.")
