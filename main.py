701ca028ef812f381f9ad3d5a29084411e765e2c
MariaFerValdivia <maria.valdivia@genstudents.org>
2024-11-26 12:56:36 -0300

    mensaje

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ nombre="Maru"
 print(f"Hola,{nombre}. Bienvenido/a a tamaworld.")
 
 nombre=input("Introduce tu nombre")
-print(f"hola {nombre}. Bienvendio a tamaworld.")
+edad=input("Introduce tu edad")
+print(f"Hola {nombre},{edad}. Bienvendio a tamaworld.")
